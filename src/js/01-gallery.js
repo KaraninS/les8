84d9aed287921c238cf2b0147f6b325f36227a64
Serhii Karanin <serjkaranin@gmail.com>
2024-01-21 20:04:15 +0200

    gallery complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const ulList = document.querySelector(".gallery");
+
+// console.log(galleryItems);
+
+const murkup = galleryItems.map(({original,description,preview}) => 
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+).join("");
+// console.log(murkup)
+ulList.insertAdjacentHTML("beforeend", murkup)
+ulList.addEventListener("click", onClick)
+function onClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) return; 
+   const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+    
+};
