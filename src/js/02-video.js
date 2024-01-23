a89ccb28ceaec274540e7fb70b9c1c33f873726d
Serhii Karanin <serjkaranin@gmail.com>
2024-01-23 21:22:23 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const player = new Player('vimeo-player');
+
+
+const onPlay = function (data) {
+   localStorage.setItem("videoplayer-current-time",data.seconds)
+};
+
+player.on('timeupdate', throttle(onPlay),1000);
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
